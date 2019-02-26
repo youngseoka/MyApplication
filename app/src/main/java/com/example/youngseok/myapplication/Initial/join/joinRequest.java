d1f78785824d4170b941191ea2d68df4905f92a4
youngseok <youngseokaaa@gmail.com>
2019-02-26 20:49:58 +0900

    서버 통신해서 중복확인 완료

---
+++ app/src/main/java/com/example/youngseok/myapplication/Initial/join/joinRequest.java
@@ -0,0 +1,31 @@
+package com.example.youngseok.myapplication.Initial.join;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class joinRequest extends StringRequest {
+    final static private String URL = "http://192.168.37.217/UserJoin.php";
+    private Map<String,String> parameters;
+
+    public joinRequest(String name, String id, String password, String nickname, String email, String phone, Response.Listener<String> listener)
+    {
+
+        super(Method.POST,URL,listener,null);
+        parameters=new HashMap<>();
+        parameters.put("name",name);
+        parameters.put("id",id);
+        parameters.put("password",password);
+        parameters.put("nickname",nickname);
+        parameters.put("email",email);
+        parameters.put("phone",phone);
+
+    }
+    @Override
+    protected Map<String,String>getParams() throws AuthFailureError{
+        return parameters;
+    }
+}
--- app/src/main/java/com/example/youngseok/myapplication/Initial/signupActivity.java
+++ app/src/main/java/com/example/youngseok/myapplication/Initial/signupActivity.java
@@ -1,18 +1,31 @@
 package com.example.youngseok.myapplication.Initial;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+import com.example.youngseok.myapplication.Initial.join.idValidate;
 import com.example.youngseok.myapplication.R;
+
+import org.json.JSONObject;
 
 public class signupActivity extends Activity {
 
     private LinearLayout layout_1, layout_2, layout_3,layout_4,layout_5,layout_6;
     private Button agree_btn,disagree_btn;
     //레이아웃들
+
+    private String name,id,password,nickname,email,phone;
+    private Boolean validate = false;   //아이디 중복확인시 boolean으로 중복확인
+
 
 
     @Override
@@ -52,5 +65,78 @@ public class signupActivity extends Activity {
                 layout_6.setVisibility(View.GONE);
             }
         });
+
+
+        final EditText name = findViewById(R.id.signup_name);
+        final EditText id = findViewById(R.id.signup_id);
+        final EditText password = findViewById(R.id.signup_passwd);
+        final EditText password_chk = findViewById(R.id.signup_passwd_chk);
+        final EditText nickname = findViewById(R.id.signup_nick);
+        final EditText email = findViewById(R.id.signup_email);
+        final EditText phone = findViewById(R.id.signup_phone);
+
+        final Button idvalidate_btn = findViewById(R.id.signup_id_btn);
+        idvalidate_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userID=id.getText().toString();
+                if(validate){
+                    return;//검증 완료시
+                }
+
+                if(userID.equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(signupActivity.this);
+                    AlertDialog dialog = builder.setMessage("아이디를 입력해주세요").setPositiveButton("ok",null).create();
+                    dialog.show();
+                    return;
+
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+
+
+                    @Override
+                    public void onResponse(String response){
+                        try{
+
+
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success =jsonResponse.getBoolean("success");
+                            if(success){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(signupActivity.this);
+                                AlertDialog dialog = builder.setMessage("사용가능합니다").setPositiveButton("ok",null).create();
+                                dialog.show();
+
+                                id.setEnabled(false);
+                                validate=true;
+
+                                idvalidate_btn.setBackgroundColor(getResources().getColor(R.color.colorGray));
+
+                            }
+                            else{
+                                AlertDialog.Builder builder=new AlertDialog.Builder(signupActivity.this);
+                                AlertDialog dialog = builder.setMessage("이미 존재하는 아이디 입니다.").setNegativeButton("ok",null).create();
+                                dialog.show();
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                idValidate idvalidate = new idValidate(userID,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(signupActivity.this);
+                queue.add(idvalidate);
+
+            }
+        });
+
+
+
+
+
+
+
     }
 }
